c8825436ecf3540aa3e09ab897edd16cdc8ff2c4
Bams <s.bumsu@gmail.com>
2023-05-23 23:12:12 +0900

    [#6] FEAT: modify url and add keyword member to crawler class

--- src/crawler.py
+++ src/crawler.py
@@ -4,44 +4,44 @@ import pandas as pd
 from datetime import datetime, timedelta
 from items import article_contents
 
-headers = {"User-Agent": "Chrome/103.0.0.0"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+}
 
 
-class NaverNewsCrawler:
-    def __init__(self, start_date, end_date, max_articles):
+class MaeilNewsCrawler:
+    def __init__(self, start_date, end_date, max_articles, keyword):
         self.start_date = start_date
         self.end_date = end_date
         self.max_articles = max_articles
         self.articles = []
         self.article_dict = {}
+        self.keyword = keyword
 
-    def extract_title_and_link(self, title, url, contents):
+    def extract_title_and_link(self, title, url, date):
         article_dict = {}
         article_dict["title"] = title
         article_dict["link"] = url
-        article_dict["contents"] = contents
+        # article_dict["contents"] = contents
+        article_dict["date"] = date
         return article_dict
 
     def find_content(self, link):
-        for domain, content_info in article_contents.items():
-            if domain in link:
-                detail_page = requests.get(
-                    link,
-                    headers=headers,
-                )
-                soup = bs(detail_page.content, "html.parser")
-                content = soup.find(*content_info)
-                if content:
-                    content_text = (
-                        content.get_text()
-                        .replace("\n", " ")
-                        .replace("\t", " ")
-                        .strip()
-                    )
-                    return content_text
-                else:
-                    return None
-        return None
+        detail_page = requests.get(
+            link,
+            headers=headers,
+        )
+        soup = bs(detail_page.content, "html.parser")
+        content = soup.select_one(
+            "#container > section > div.news_detail_body_group > section > div.min_inner > div.sec_body > div.news_cnt_detail_wrap"
+        )
+        if content:
+            content_text = (
+                content.get_text().replace("\n", " ").replace("\t", " ").strip()
+            )
+            return content_text
+        else:
+            return None
 
     def crawl_news(self):
         page = 1
@@ -49,38 +49,34 @@ class NaverNewsCrawler:
         while count == 1:
             if len(self.articles) >= self.max_articles:
                 break
-
-            url = f"https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=101&listType=summary&range={self.end_date.strftime('%Y%m%d')}|{self.start_date.strftime('%Y%m%d')}&page={page}"
+            url = f"https://find.mk.co.kr/new/search.php?pageNum={page}&cat=&cat1=&media_eco=&pageSize=20&sub=news&dispFlag=OFF&page=news&s_kwd={self.keyword}&s_page=news&go_page=&ord=1&ord1=1&ord2=0&s_keyword={self.keyword}&s_i_keyword={self.keyword}&s_author=&y1=2020&m1=05&d1=23&y2=2023&m2=05&d2=23&ord=1&area=ttbd"
+            print(url)
             res = requests.get(url, headers=headers)
+            print(res)
             soup = bs(res.content, "html.parser")
-            news_list = soup.find("ul", {"class": "type06_headline"}).find_all(
-                "li", {"class": ""}
-            )
-            news_list += soup.find("ul", {"class": "type06"}).find_all(
-                "li", {"class": ""}
-            )
-
+            news_list = soup.find_all("div", {"class": "sub_list"})
             for news in news_list:
                 print(len(self.articles))
                 if len(self.articles) >= self.max_articles:
                     break
                 article_link = news.find("a")["href"]
-                article_title = news.find("img")
-                if article_title is None:
-                    article_title = news.find("a").text.strip()
-                else:
-                    article_title = news.find("img")["alt"].strip()
+                article_title = news.find(
+                    "span", {"class": "art_tit"}
+                ).text.strip()
+                article_date = news.find(
+                    "span", {"class": "art_time"}
+                ).text.strip()
                 print(article_title)
                 print(article_link)
-                article_contents = self.find_content(article_link)
-                if article_contents is not None:
-                    article_dict = self.extract_title_and_link(
-                        article_title, article_link, article_contents
-                    )
-                    self.articles.append(article_dict)
-                    self.article_dict[article_dict["title"]] = article_link
+                article_dict = self.extract_title_and_link(
+                    article_title,
+                    article_link,
+                    article_date,
+                )
+                self.articles.append(article_dict)
+                self.article_dict[article_dict["title"]] = article_link
             page += 1
 
     def save_to_csv(self, file_name):
-        df = pd.DataFrame(self.articles, columns=["title", "link", "contents"])
+        df = pd.DataFrame(self.articles, columns=["title", "link", "date"])
         df.to_csv(file_name, index=False, encoding="utf-8-sig")
--- src/test_crawling.py
+++ src/test_crawling.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
-from crawler import NaverNewsCrawler
+from crawler import MaeilNewsCrawler
 
 start_date = datetime(2022, 1, 1)
 end_date = datetime(2022, 4, 24)
 
-crawler = NaverNewsCrawler(start_date, end_date, max_articles=1000)
+crawler = MaeilNewsCrawler(
+    start_date, end_date, max_articles=1000, keyword="채권"
+)
 crawler.crawl_news()
-crawler.save_to_csv("naver_news.csv")
+crawler.save_to_csv("mk_news.csv")
